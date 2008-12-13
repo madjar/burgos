a879866d9c3d3f008b45b0da39122b4ddcf5e806
serianox <>
2008-12-13 23:50:32 +0000

    Commit à partir de Fedora *La compilation passe très bien, et le tray semble bien fonctionner *Ajout d'un peu de localisation, pour tester ;) **TODO** *Poursuivre la localisation ...

---
+++ burgos_fr_FR.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="fr">
+<context>
+    <name>Burgos</name>
+    <message>
+        <location filename="burgos.cpp" line="168"/>
+        <source>&amp;Ouvrir</source>
+        <translation>&amp;Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="burgos.cpp" line="170"/>
+        <source>&amp;Quitter</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="burgos.ui" line="14"/>
+        <source>Burgos</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="burgos.ui" line="27"/>
+        <source>Tree View</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="burgos.ui" line="53"/>
+        <source>Peer View</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="burgos.ui" line="66"/>
+        <source>Log View</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Model</name>
+    <message>
+        <location filename="model.cpp" line="89"/>
+        <source>Nom</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="91"/>
+        <source>Taille</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>PeerModel</name>
+    <message>
+        <location filename="peermodel.cpp" line="82"/>
+        <source>Pair</source>
+        <comment>peer hostname or ip address</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="peermodel.cpp" line="86"/>
+        <source>Informations</source>
+        <comment>peer informations</comment>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="peermodel.cpp" line="97"/>
+        <source>%n pair(s) disponible(s)</source>
+        <comment>other users are found</comment>
+        <translation>
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="peermodel.cpp" line="102"/>
+        <source>Aucun pairs disponibles</source>
+        <comment>no other users are found</comment>
+        <translation></translation>
+    </message>
+</context>
+</TS>
