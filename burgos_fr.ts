f716df22702a89ec3a42d9b1c57cb9ff8ea00d59
Georges Dubus <georges.dubus@supelec.fr>
2009-02-07 11:58:41 +0100

    Mise à jour du fichier de traduction pour correspondre aux nouveaux chemins

--- burgos_fr.ts
+++ burgos_fr.ts
@@ -3,62 +3,62 @@
 <context>
     <name>Burgos</name>
     <message>
-        <location filename="burgos.ui" line="14"/>
+        <location filename="src/burgos.ui" line="14"/>
         <source>Burgos</source>
         <translation>Burgos</translation>
     </message>
     <message>
-        <location filename="burgos.cpp" line="39"/>
+        <location filename="src/burgos.cpp" line="39"/>
         <source>Burgos is running</source>
         <translation>Burgos est lancé</translation>
     </message>
     <message>
-        <location filename="burgos.cpp" line="132"/>
+        <location filename="src/burgos.cpp" line="132"/>
         <source>Burgos is now reduced</source>
         <translation>Burgos est maintenant réduit</translation>
     </message>
     <message>
-        <location filename="burgos.cpp" line="40"/>
+        <location filename="src/burgos.cpp" line="40"/>
         <source>Anyway, this message is going to disapear sonner or latter.</source>
         <translation>Et c&apos;est tray bien.</translation>
     </message>
     <message>
-        <location filename="burgos.cpp" line="133"/>
+        <location filename="src/burgos.cpp" line="133"/>
         <source>Burgos is still running and refreshing the list of ftps</source>
         <translation>Burgos continue de tourner en tâche de fond pour actualiser la liste des ftp</translation>
     </message>
     <message>
-        <location filename="burgos.ui" line="41"/>
+        <location filename="src/burgos.ui" line="41"/>
         <source>Scan</source>
         <translation>Scanner</translation>
     </message>
     <message>
-        <location filename="burgos.cpp" line="184"/>
+        <location filename="src/burgos.cpp" line="184"/>
         <source>&amp;Hide</source>
         <translation>&amp;Cacher</translation>
     </message>
     <message>
-        <location filename="burgos.cpp" line="159"/>
+        <location filename="src/burgos.cpp" line="159"/>
         <source>&amp;Show</source>
         <translation>&amp;Afficher</translation>
     </message>
     <message>
-        <location filename="burgos.cpp" line="186"/>
+        <location filename="src/burgos.cpp" line="186"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="burgos.ui" line="27"/>
+        <location filename="src/burgos.ui" line="27"/>
         <source>&amp;FTP servers</source>
         <translation>Serveurs &amp;FTP</translation>
     </message>
     <message>
-        <location filename="burgos.ui" line="57"/>
+        <location filename="src/burgos.ui" line="57"/>
         <source>&amp;Peers</source>
         <translation>&amp;Pairs</translation>
     </message>
     <message>
-        <location filename="burgos.ui" line="70"/>
+        <location filename="src/burgos.ui" line="70"/>
         <source>&amp;Log</source>
         <translation>&amp;Historique</translation>
     </message>
@@ -66,7 +66,7 @@
 <context>
     <name>Cli</name>
     <message>
-        <location filename="cli.cpp" line="41"/>
+        <location filename="src/cli.cpp" line="48"/>
         <source>Unknown command : %1</source>
         <translation>Commande inconnue : %1</translation>
     </message>
@@ -74,7 +74,7 @@
 <context>
     <name>Ftp</name>
     <message>
-        <location filename="ftp.cpp" line="110"/>
+        <location filename="src/ftp.cpp" line="110"/>
         <source>&quot;%1&quot; is not a utf8 string, behavior is not guaranteed.</source>
         <translation>&quot;%1&quot; n&apos;est pas une chaîne utf8, le comportement n&apos;est pas garanti.</translation>
     </message>
@@ -82,17 +82,17 @@
 <context>
     <name>FtpModel</name>
     <message>
-        <location filename="ftpmodel.cpp" line="110"/>
+        <location filename="src/ftpmodel.cpp" line="110"/>
         <source>Name</source>
         <translation>Nom</translation>
     </message>
     <message>
-        <location filename="ftpmodel.cpp" line="112"/>
+        <location filename="src/ftpmodel.cpp" line="112"/>
         <source>Size</source>
         <translation>Taille</translation>
     </message>
     <message>
-        <location filename="ftpmodel.cpp" line="158"/>
+        <location filename="src/ftpmodel.cpp" line="158"/>
         <source>Forty two</source>
         <translation>Quarante-deux</translation>
     </message>
@@ -100,19 +100,19 @@
 <context>
     <name>PeerModel</name>
     <message>
-        <location filename="peermodel.cpp" line="82"/>
+        <location filename="src/peermodel.cpp" line="82"/>
         <source>Peer</source>
         <comment>peer hostname or ip address</comment>
         <translation>Pair</translation>
     </message>
     <message>
-        <location filename="peermodel.cpp" line="86"/>
+        <location filename="src/peermodel.cpp" line="86"/>
         <source>Information</source>
         <comment>peer informations</comment>
         <translation>Informations</translation>
     </message>
     <message numerus="yes">
-        <location filename="peermodel.cpp" line="97"/>
+        <location filename="src/peermodel.cpp" line="97"/>
         <source>%n peer(s) available</source>
         <comment>other users are found</comment>
         <translation>
@@ -121,7 +121,7 @@
         </translation>
     </message>
     <message>
-        <location filename="peermodel.cpp" line="102"/>
+        <location filename="src/peermodel.cpp" line="102"/>
         <source>No peers available</source>
         <comment>no other users are found</comment>
         <translation>Pas de pairs disponibles</translation>
